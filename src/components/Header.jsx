b90451734af010cfef1e2d8cd3f050f6f65ad259
Robert Walker <robertWalker6580@gmail.com>
2025-05-07 20:41:56 -0700

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+export default function Header() {
+  return (
+    <div className='mx-auto flex max-w-6xl items-center justify-between p-3'>
+      <ul className='flex gap-4'>
+        <li>
+          <Link href='/sign-in'>Sign in</Link>
+        </li>
+        <li className='hidden sm:block'>
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li className='hidden sm:block'>
+          <Link href='/about'>About</Link>
+        </li>
+      </ul>
+      <Link
+        href={'/'}
+        className='flex items-center gap-1'
+      >
+        <span className='rounded-lg bg-amber-500 px-2 py-1 text-2xl font-bold'>
+          IMDb
+        </span>
+        <span className='hidden text-xl sm:inline'>Clone</span>
+      </Link>
+    </div>
+  );
+}
